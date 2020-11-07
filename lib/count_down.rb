0c5e740aa089e23269f1eb3de8a598bb455bd025
Jeff Golden <gjgolden08@gmail.com>
2020-11-07 21:32:28 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,8 +1,9 @@
 happy_new_year = 0 
-count = 11
+count = 10
 
 while count > 0 && count != happy_new_year do
   break if count == happy_new_year
   puts "#{count}"
   count -= 1 
 end
+puts happy_new_year
